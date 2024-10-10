78de9787654ceb3a809e78862f5a1ed0b60ba682
maximbrail <maxabobus953@gmail.com>
2024-10-10 20:22:53 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-const links = document.querySelector(".links")
-const burger = document.querySelector(".burger")
+const burger = document.querySelector('.burger')
+const nav = document.querySelector('.nav')
 
-burger.onclick = () => {
-    links.classList.toggle('open')
-}
+burger.addEventListener('click', () => {
+  nav.classList.toggle('active')
+})
+
+
+
+
+
+
+const swiperAlbum = new Swiper('.album-swiper-1', {
+  loop: true,
+  slidesPerView: 0.5,
+  speed: 20000,
+  spaceBetween: 10,
+  allowTouchMove: false,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
+});
+
+
+const swiperAlbum2 = new Swiper('.album-swiper-2', {
+  loop: true,
+  slidesPerView: 0.5,
+  speed: 20000,
+  spaceBetween: 10,
+  allowTouchMove: false,
+  autoplay: {
+    reverseDirection: true,
+    delay: 0,
+    disableOnInteraction: false,
+  },
+});
